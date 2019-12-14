896dc84afe5d389bfdf75c06ea8f33910c238637
Meghan Gunderson <megundersonmn@gmail.com>
2019-12-14 14:10:42 -0600

    added readyNow and beginning of addEmployee functions

--- client.js
+++ client.js
@@ -1,1 +1,14 @@
 console.log('js');
+
+//Jquery ready 
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('readyNow');
+    // add click listeners
+    $('#submitBtn').on('click', addEmployee);
+}
+
+function addEmployee(){
+    console.log('subitBtn works!');
+}
